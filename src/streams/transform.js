bb1591713f4807cc859e6ec91171a916cb8c1c13
NMakarevich <nikoladze529@mail.ru>
2025-04-24 20:11:30 +0300

    feat: implement streams part of task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,35 @@
+import { stdin, stdout } from "node:process";
+import { pipeline } from 'node:stream/promises'
+
+import { Transform, Readable, Writable } from 'node:stream';
+
+class ReadableStream extends Readable {
+    _read(size) {
+        stdin.on('data', (chunk) => this.push(chunk))
+    }
+}
+
+class WritableStream extends Writable {
+    _write(chunk, encoding, callback) {
+        stdout.write(`${chunk}\n`);
+        callback();
+    }
+}
+
+class ReverseTransform extends Transform {
+    _transform(chunk, encoding, callback) {
+        const result = chunk.toString().split('').reverse().join('');
+        this.push(result);
+        callback();
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new ReverseTransform()
+    const readable = new ReadableStream();
+    const writable = new WritableStream();
+
+    await pipeline(readable, reverseTransform, writable)
 };
 
 await transform();
