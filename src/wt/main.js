b24898dfbf9494a098ebd1b612ee4f572227d85e
NMakarevich <nikoladze529@mail.ru>
2025-04-25 01:03:59 +0300

    feat: implement wt part of task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { cpus } from "node:os";
+import { Worker, isMainThread } from 'node:worker_threads'
+import * as path from "node:path";
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCoresNumber = cpus().length;
+    if (isMainThread) {
+        const promises = []
+        for (let i = 0; i < cpuCoresNumber; i += 1) {
+            promises.push(new Promise((resolve) => {
+                const worker = new Worker(path.join(import.meta.dirname, 'worker.js'))
+                worker.postMessage(10 + i);
+                worker.on('message', (data) => {
+                    resolve({ status: 'resolved', data })
+                });
+                worker.on("error", () => {
+                    resolve({ status: 'error', data: null })
+                });
+            }))
+
+        }
+        const results = await Promise.all(promises);
+        console.log(results)
+    }
 };
 
 await performCalculations();
