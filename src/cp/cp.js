8f6f024361a980c07de61b6a688fb4a24f8637ce
NMakarevich <nikoladze529@mail.ru>
2025-04-25 13:45:18 +0300

    feat: implement cp part of task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fork } from 'node:child_process';
+import * as path from "node:path";
+
+const CHILD = 'script.js'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(path.join(import.meta.dirname, 'files', CHILD), args);
+    childProcess.on("data", (data) => {
+        childProcess.send(data)
+    })
+    childProcess.on("message", (data) => {
+        console.log(data)
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
